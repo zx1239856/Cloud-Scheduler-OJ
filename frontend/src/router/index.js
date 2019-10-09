0b8e523ef8dd9f972940c8484ce11b9965689bf5
JinwooSeong <seongjinwoo76@yahoo.com>
2019-10-09 19:48:01 +0800

    feat(dev-czy): web ide component added, it supports highlights of some programming languages, file trees, and some additional stuff. Waiting for more improvement.

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,7 @@ import Router from 'vue-router';
 import HelloWorld from '@/components/HelloWorld';
 import Login from '@/components/Login';
 import WebSSH from '@/components/WebSSH';
+import WebIDE from '@/components/WebIDE';
 
 export default new Router({
     mode: 'history',
@@ -21,6 +22,11 @@ export default new Router({
             path: '/web-ssh',
             name: 'WebSSH',
             component: WebSSH
+        },
+        {
+            path: '/web-ide',
+            name: 'WebIDE',
+            component: WebIDE
         }
     ]
 });
