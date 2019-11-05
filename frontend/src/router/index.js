16cf68ef8f8b48325eaf50141c62d2de0e997ab2
zx1239856 <zx1239856@gmail.com>
2019-11-05 20:32:48 +0800

    feat(dev-zx): Support NoVNC frontend, now user can connect to a running VNC pod easily (#130)

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -99,6 +99,13 @@ export const constantRoutes = [
                 hidden: true,
                 component: () => import('@/views/webide/index'),
                 meta: { title: 'Web IDE' }
+            },
+            {
+                path: 'vnc-view/',
+                name: 'vnc',
+                hidden: true,
+                component: () => import('@/views/vnc/index'),
+                meta: { title: 'VNC Viewer' }
             }
         ]
     },
