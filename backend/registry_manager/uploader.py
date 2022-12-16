dc542a3a4e2641ebe47244698aba15986f35883c
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-16 22:44:07 +0000

    Adding tarfile member sanitization to extractall()

--- backend/registry_manager/uploader.py
+++ backend/registry_manager/uploader.py
@@ -20,7 +20,26 @@ class DockerTarUploader:
     @staticmethod
     def _extract_tar_file(tar, path):
         with tarfile.open(tar) as tar_file:
-            tar_file.extractall(path)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar_file, path)
 
     @staticmethod
     def _create_manifest(conf_path, layer_paths):
